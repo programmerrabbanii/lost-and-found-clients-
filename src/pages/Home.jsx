302e7b0ea922ec8e6098e93558d83af058929ba0
programmerrabbanii <developerrabbanisarker@gmail.com>
2024-12-22 22:23:55 +0600

    added the get items in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Bannar from "../components/Bannar";
 import HowItWork from "../components/HowItWork";
+import LostItems from "../components/LostItems";
 import Success from "../components/Success";
 
 const Home = () => {
     return (
         <div>
             <Bannar></Bannar>
+            <LostItems></LostItems>
              <HowItWork></HowItWork>
              <Success></Success>
              
