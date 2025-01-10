e062e34f2622e19d6e08ba9bc59ccc3e17da8172
programmerrabbanii <developerrabbanisarker@gmail.com>
2025-01-10 18:18:23 +0600

    recover items cart

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,98 +15,50 @@ const Navbar = () => {
 
         {/* Navigation Links */}
         <div className='flex items-center space-x-4'>
-          <ul className='hidden md:flex space-x-4'>
+          <ul className='flex space-x-4'>
+            {/* All Routes */}
             <li>
               <Link to='/' className='hover:text-gray-200'>Home</Link>
             </li>
             <li>
               <Link to='/found' className='hover:text-gray-200'>Lost & Found Items</Link>
             </li>
+            <li>
+              <Link to='/add-lost' className='hover:text-gray-200'>Add Lost Item</Link>
+            </li>
+            <li>
+              <Link to='/recovered' className='hover:text-gray-200'>Recovered Items</Link>
+            </li>
+            <li>
+              <Link to='/my-manage' className='hover:text-gray-200'>Manage My Items</Link>
+            </li>
 
-            {!user && (
+            {!user ? (
               <li>
                 <Link to='/login' className='hover:text-gray-200'>Login</Link>
               </li>
+            ) : (
+              <>
+                {/* User Profile Image and Logout Button */}
+                <div className='flex items-center space-x-4'>
+                  <div className='flex items-center'>
+                    <img
+                      referrerPolicy='no-referrer'
+                      alt='User Profile'
+                      src={user?.photoURL}
+                      className='w-10 h-10 rounded-full'
+                    />
+                    <button
+                      onClick={logOutUser}
+                      className='bg-green-700 hover:bg-green-800 text-white py-1 px-4 rounded ml-2'
+                    >
+                      Logout
+                    </button>
+                  </div>
+                </div>
+              </>
             )}
           </ul>
-
-          {/* Mobile Menu Toggle */}
-          <div className='dropdown dropdown-end md:hidden'>
-            <label tabIndex={0} className='btn btn-ghost'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                fill='none'
-                viewBox='0 0 24 24'
-                strokeWidth={1.5}
-                stroke='currentColor'
-                className='w-6 h-6'
-              >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  d='M3.75 6.75h16.5m-16.5 5.25h16.5m-16.5 5.25h16.5'
-                />
-              </svg>
-            </label>
-            <ul
-              tabIndex={0}
-              className='menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 text-black'
-            >
-              <li>
-                <Link to='/'>Home</Link>
-              </li>
-              <li>
-                <Link to='/found'>Lost & Found Items</Link>
-              </li>
-              {!user && (
-                <li>
-                  <Link to='/login'>Login</Link>
-                </li>
-              )}
-            </ul>
-          </div>
-
-          {/* User Profile */}
-          {user && (
-            <div className='dropdown dropdown-end'>
-              <div
-                tabIndex={0}
-                role='button'
-                className='btn btn-ghost btn-circle avatar tooltip tooltip-bottom'
-                data-tip={user?.displayName || 'User'}
-              >
-                <div className='w-10 rounded-full'>
-                  <img
-                    referrerPolicy='no-referrer'
-                    alt='User Profile'
-                    src={user?.photoURL}
-                  />
-                </div>
-              </div>
-              <ul
-                tabIndex={0}
-                className='menu menu-sm dropdown-content mt-3 p-2 shadow bg-green-900 rounded-box w-52'
-              >
-                <li>
-                  <Link to='/add-lost'>Add Lost & Found Item</Link>
-                </li>
-                <li>
-                  <Link to='/recovered'>All Recovered Items</Link>
-                </li>
-                <li>
-                  <Link to='/my-manage'>Manage My Items</Link>
-                </li>
-                <li>
-                  <button
-                    onClick={logOutUser}
-                    className='bg-green-700 hover:bg-green-800 text-white py-1 px-4 rounded'
-                  >
-                    Logout
-                  </button>
-                </li>
-              </ul>
-            </div>
-          )}
         </div>
       </div>
     </nav>
